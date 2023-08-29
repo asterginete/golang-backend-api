a04c62225e93d7a90cd714f36ed0966db71a1763
Aster Ginete <asterginete@yahoo.com>
2023-08-29 09:25:21 +0900

    add authentication

---
+++ internal/handler/auth.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"your_project_path/internal/util"
+)
+
+func Login(c *gin.Context) {
+	var credentials struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.BindJSON(&credentials); err != nil {
+		c.JSON(400, gin.H{"error": "Bad request"})
+		return
+	}
+
+	// For simplicity, we're hardcoding a username and password.
+	// In a real-world scenario, you'd fetch and verify this from a database.
+	if credentials.Username == "admin" && credentials.Password == "password" {
+		token, err := util.GenerateToken(credentials.Username)
+		if err != nil {
+			c.JSON(500, gin.H{"error": "Internal Server Error"})
+			return
+		}
+		c.JSON(200, gin.H{"token": token})
+	} else {
+		c.JSON(401, gin.H{"error": "Authentication failed"})
+	}
+}
---
+++ internal/util/jwt.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+const (
+	SECRET_KEY = "your_secret_key" // Change this to a more secure key
+)
+
+func GenerateToken(username string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = username
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix() // Token expires after 72 hours
+
+	return token.SignedString([]byte(SECRET_KEY))
+}
